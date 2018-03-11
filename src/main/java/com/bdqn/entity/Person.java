df9cf9fb7fe1cf471ed379fac11e053d1f7d168f
zulongao123 <zlh891116@sina.com>
2018-03-11 21:51:57 +0800

    修改Person类

--- src/main/java/com/bdqn/entity/Person.java
+++ src/main/java/com/bdqn/entity/Person.java
@@ -5,4 +5,23 @@ package com.bdqn.entity;
  */
 public class Person {
     private String name,age;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
